d5baa8d28f8a8d7c526e43db7011465d756c0b3d
RicBM71 <rbartolome@sanaval.com>
2020-05-11 17:07:29 +0200

    google analitycs

--- resources/js/plugins/router.js
+++ resources/js/plugins/router.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics';
 
 import Home from '../components/Home.vue';
 
 Vue.use(VueRouter)
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+    id: 'UA-1457871-2',
+    router
+  });
+
 
 
 let routes = [
@@ -13,5 +21,6 @@ let routes = [
 
 export default  new VueRouter({
     mode: 'history',
-    routes // short for `routes: routes`
+    routes, // short for `routes: routes`
+    render: h => h(App)
 });
